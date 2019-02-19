edacfe54d6f5892e65eaac82a12d908b8ab82de2
gulnaz8891 <gulnaz.tor@gmail.com>
2019-02-19 13:15:07 -0500

    task1

---
+++ main.py
@@ -0,0 +1,10 @@
+from myclass import Rectangle
+
+R2=Rectangle("blue",1,15)
+print(R2.color)
+print(R2.square())
+print(R2.perimetr())
+from myclass import Color
+c1=Color(['pink','blue','red'])
+print("len", len(c1))
+
